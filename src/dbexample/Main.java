6a2ca0bc4c0f5c985025df630e2fa5f86e174446
kafedra-iust <kafedra.iust@gmail.com>
2019-12-23 19:51:16 +0200

    Initial commit

---
+++ src/dbexample/Main.java
@@ -0,0 +1,62 @@
+package dbexample;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run1();
+    }
+
+    private void run() {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "123")) {
+            PreparedStatement ps = connection.prepareStatement("update example.example.customer set sum = ? where sum < ?");
+            ps.setInt(2, 1000);
+            ps.setInt(1, 500);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void run3() {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "123")) {
+            PreparedStatement ps = connection.prepareStatement("delete from example.example.customer where sum = ?");
+            ps.setInt(1, 0);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void run2() {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "123")) {
+            PreparedStatement ps = connection.prepareStatement("insert into example.customer (name, sum, card_number) values (?,?,?)");
+            ps.setString(1, "Alexeev");
+            ps.setInt(2, 1000);
+            ps.setInt(3, 1256);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void run1() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "123");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from example.customer");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int sum = rs.getInt("sum");
+                int cardNumber = rs.getInt("card_number");
+                System.out.println(id + " : " + name + " : " + sum + " : " + cardNumber);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
